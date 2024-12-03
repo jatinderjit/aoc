c1e34f8849504bb4af7fedab98d286f86ccaa112
Jatinderjit Singh <jatinderjit89@gmail.com>
2024-12-04 03:44:36 +0530

    Solve Day 1 - Part 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,7 +1,30 @@
-use std::{error::Error, fs::File, io::Read};
+use std::{collections::HashMap, error::Error, fs::File, io::Read};
 
-pub fn solve(inp: &str) -> Result<i32, Box<dyn Error>> {
-    let mut f = File::open(inp)?;
+pub fn solve() {
+    let (mut left, mut right) = read_input("inputs/day1.txt").unwrap();
+    println!("Part 1: {}", distance(&mut left, &mut right));
+    println!("Part 2: {}", similarity(&mut left, &mut right));
+}
+
+fn distance(left: &mut [i32], right: &mut [i32]) -> i32 {
+    left.sort();
+    right.sort();
+    left.iter().zip(right).map(|(l, r)| (*l - *r).abs()).sum()
+}
+
+fn similarity(left: &mut [i32], right: &mut [i32]) -> i32 {
+    let mut counter = HashMap::new();
+    for num in right {
+        let entry = counter.entry(*num).or_insert(0);
+        *entry += 1;
+    }
+    left.iter()
+        .map(|v| v * counter.get(v).copied().unwrap_or(0))
+        .sum()
+}
+
+fn read_input(input_path: &str) -> Result<(Vec<i32>, Vec<i32>), Box<dyn Error>> {
+    let mut f = File::open(input_path)?;
 
     let mut left = Vec::new();
     let mut right = Vec::new();
@@ -21,24 +44,24 @@ pub fn solve(inp: &str) -> Result<i32, Box<dyn Error>> {
             right.push(values[1]);
         });
 
-    let ans = distance(&mut left, &mut right);
-    Ok(ans)
-}
-
-fn distance(left: &mut [i32], right: &mut [i32]) -> i32 {
-    left.sort();
-    right.sort();
-    left.iter().zip(right).map(|(l, r)| (*l - *r).abs()).sum()
+    Ok((left, right))
 }
 
 #[cfg(test)]
 mod test {
-    use super::distance;
+    use super::*;
 
     #[test]
     fn test_distance() {
         let mut left = vec![3, 4, 2, 1, 3, 3];
         let mut right = vec![4, 3, 5, 3, 9, 3];
         assert_eq!(distance(&mut left, &mut right), 11);
+    }
+
+    #[test]
+    fn test_similarity() {
+        let mut left = vec![3, 4, 2, 1, 3, 3];
+        let mut right = vec![4, 3, 5, 3, 9, 3];
+        assert_eq!(similarity(&mut left, &mut right), 31);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use aoc24::day1;
 
 fn main() {
-    let ans = day1::solve("inputs/day1.txt").unwrap();
-    println!("{ans}");
+    day1::solve();
 }
